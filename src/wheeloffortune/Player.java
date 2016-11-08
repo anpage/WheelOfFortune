3bab184292e9a21c0f58399ddf31e3f20657b099
Alex Page <alex@anpage.me>
2016-11-08 16:38:40 -0500

    Implement Player class

--- src/wheeloffortune/Player.java
+++ src/wheeloffortune/Player.java
@@ -10,5 +10,32 @@ package wheeloffortune;
  * @author Alex
  */
 public class Player {
-    
+    String name;
+    int gameCash;
+    int roundCash;
+
+    public Player(String newName) {
+        name = newName;
+    }
+
+    public void newRound() {
+        gameCash += roundCash;
+        roundCash = 0;
+    }
+
+    public void bankrupt() {
+        roundCash = 0;
+    }
+
+    public void giveCash(int cash) {
+        roundCash += cash;
+    }
+
+    public int getGameCash() {
+        return gameCash;
+    }
+
+    public int getRoundCash() {
+        return roundCash;
+    }
 }
