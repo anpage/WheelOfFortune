e63d33481217a972fe5e2a85d2cf639f0d6c6d71
Alex Page <alex@anpage.me>
2016-11-22 17:00:47 -0500

    Implement Puzzle class

--- src/wheeloffortune/Puzzle.java
+++ src/wheeloffortune/Puzzle.java
@@ -11,9 +11,54 @@ package wheeloffortune;
  */
 public class Puzzle {
     private int number;
-    private String solution;
     private String category;
+    private String solution;
     private String guessed;
-    
-    
+
+    public Puzzle() { // TODO: Implement real data
+        number = 0;
+        category = "Animals";
+        solution = "Kitty Cats";
+        guessed = " ";
+    }
+
+    public int makeGuess(char ch) {
+        // Convert to lowercase string to make case insensitve
+        String guess = Character.toString(Character.toLowerCase(ch));
+
+        // Append to guessed string if new guess,
+        // otherwise return -1 to indicate existing guess
+        if (guessed.indexOf(guess) == -1){
+            guessed += guess;
+        } else {
+            return -1;
+        }
+
+        // Return the number of occurences in the solution
+        return solution.length() - solution.replaceAll(guess, "").length();
+    }
+
+    public String getState() {
+        // Build a new string, replacing non-guessed chars with '*'
+        String obscuredSolution = "";
+
+        for (int i = 0; i < solution.length(); i++) {
+            char ch = solution.charAt(i);
+            if (guessed.indexOf(Character.toLowerCase(ch)) == -1) {
+                obscuredSolution += '*';
+            } else {
+                obscuredSolution += ch;
+            }
+        }
+
+        return obscuredSolution;
+    }
+
+    public boolean solvePuzzle(String input) {
+        return input.equalsIgnoreCase(solution);
+    }
+
+    public String getCategory() {
+        return category;
+    }
 }
